2dac0c1b49c024297274d05cba6a7427c4fb72cf
whh1020 <34291711+whh1020@users.noreply.github.com>
2019-06-24 15:27:08 +0800

    Add files via upload

---
+++ src/主/java/baseFunction/dao/IBaseFunctionDao.java
@@ -0,0 +1,35 @@
+package com.qhit.baseFunction.dao;
+
+import com.qhit.baseFunction.pojo.BaseFunction;
+import com.qhit.utils.BaseDao;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+* Created by GeneratorCode on 2018/10/28
+*/
+@Repository
+public interface IBaseFunctionDao {
+
+    boolean insert(Object object);
+
+    boolean  update(Object object);
+
+    boolean  updateSelective(Object object);
+
+    boolean delete(Object object);
+
+    List freeFind(String sql);
+
+    List findAll();
+
+    List findById(Object id);
+
+    List findByFname(Object fname);
+
+    List<BaseFunction> findByMid(Integer mid,Integer userId);
+
+    List findByUrl(Object url);
+
+}
