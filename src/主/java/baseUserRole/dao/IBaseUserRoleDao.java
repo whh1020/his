f325423a1fc3305bb422b1bf6e71cada187b1037
whh1020 <34291711+whh1020@users.noreply.github.com>
2019-06-24 16:00:36 +0800

    Add files via upload

---
+++ src/主/java/baseUserRole/dao/IBaseUserRoleDao.java
@@ -0,0 +1,36 @@
+package com.qhit.baseUserRole.dao;
+
+import org.springframework.stereotype.Repository;
+import com.qhit.baseUserRole.pojo.BaseUserRole;
+import java.util.List;
+
+/**
+* Created by GeneratorCode on 2018/12/24
+*/
+
+@Repository  
+public interface IBaseUserRoleDao {
+
+    boolean insert(Object object);
+
+    boolean  update(Object object);
+
+    boolean  updateSelective(Object object);
+
+    boolean delete(Object object);
+
+    List freeFind(String sql);
+
+    List findAll();
+
+    List findById(Object id);
+
+    boolean freeUpdate(String sql);
+
+    List<BaseUserRole> search(BaseUserRole baseUserRole);
+
+    List findByUid(Object uid);
+
+    List findByRid(Object rid);
+
+}
