634e1077160e7cf1d7641571f2f5c828afb21a52
whh1020 <34291711+whh1020@users.noreply.github.com>
2019-06-24 15:31:06 +0800

    Add files via upload

---
+++ src/主/java/baseRoleFunction/service/IBaseRoleFunctionService.java
@@ -0,0 +1,25 @@
+package com.qhit.baseRoleFunction.service;
+
+import java.util.List;
+import com.qhit.baseRoleFunction.pojo.BaseRoleFunction;
+/**
+* Created by GeneratorCode on 2018/12/24
+*/
+
+public interface IBaseRoleFunctionService {
+
+    boolean insert(Object object);
+
+    boolean  update(Object object);
+
+    boolean  updateSelective(Object object);
+
+    boolean delete(Object id);
+
+    List findAll();
+
+    BaseRoleFunction findById(Object id);
+
+    List<BaseRoleFunction> search(BaseRoleFunction baseRoleFunction);
+
+}
