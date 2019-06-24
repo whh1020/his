54ce3b7c545e80911d37d685e49c814abf29af93
whh1020 <34291711+whh1020@users.noreply.github.com>
2019-06-24 15:31:45 +0800

    Add files via upload

---
+++ src/主/java/baseUser/service/IBaseUserService.java
@@ -0,0 +1,38 @@
+package com.qhit.baseUser.service;
+
+import java.util.List;
+
+import com.qhit.baseRole.pojo.BaseRole;
+import com.qhit.baseUser.pojo.BaseUser;
+/**
+* Created by GeneratorCode on 2018/10/10
+*/
+
+public interface IBaseUserService {
+
+    boolean insert(Object object);
+
+    boolean  update(Object object);
+
+    boolean  updateSelective(Object object);
+
+    boolean delete(Object id);
+
+    List findAll();
+
+    BaseUser findById(Object id);
+
+    List<BaseUser> search(BaseUser baseUser);
+
+    BaseUser login(BaseUser baseUser);
+
+    boolean findOldPassword(BaseUser baseUser);
+
+    List<BaseRole> findLeftRole(Integer userId);
+
+    List<BaseRole> findRightRole(Integer userId);
+
+    void distributeUpdate(Integer userId, String[] rids);
+
+    List<BaseUser> findByDeptId(Integer deptId);
+}
